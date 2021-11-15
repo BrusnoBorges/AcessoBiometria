a9b88ef8b0d485e16c940329b8778b808cda32c2
BrusnoBorges <Brunosbsg@gmail.com>
2021-11-15 01:06:47 -0300

    Add project files.

---
+++ Controller/Biometria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using XnaFan.ImageComparison;
+
+namespace AcessoBiometria.Controller
+{
+    public class Biometria
+    {
+        string imgEnviada;
+        string imgCompare;
+
+        public bool Acesso(string caminho, string caminho2)
+        {           
+            return Compara(caminho, caminho2);
+        }
+
+        public bool Cadastro()
+        {
+            return false;
+        }
+
+
+        private bool Compara(string caminho, string caminho2)
+        {
+            Image img1 = Image.FromFile(caminho);
+            Image img2 = Image.FromFile(caminho2);
+
+            float dife = ImageTool.GetPercentageDifference(caminho, caminho, 3);
+
+            return dife == 0 ? false : true;
+        }
+        
+    }
+}
---
+++ View/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AcessoBiometria.Controller;
+
+namespace AcessoBiometria
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFile = new OpenFileDialog();
+        Biometria biometria = new Biometria();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            biometria.Acesso(SelectFile(), SelectFile());
+        }
+
+        public string SelectFile()
+        {
+            openFile.InitialDirectory = "c:\\";
+            openFile.Filter = "Database files (*.png, *.jpg)|*.png;*.jpg";
+            openFile.FilterIndex = 0;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                return openFile.FileName;                
+            }
+            return "";
+        }
+    }
+}
